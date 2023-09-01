2ba9a15901c6fcb50146dafa52b03f5df2f9d46c
AllanMuhari <allanmuhari087@gmail.com>
2023-09-01 19:15:15 +0300

    migrations

--- migrations/migrations.go
+++ migrations/migrations.go
@@ -2,51 +2,65 @@ package migrations
 
 import (
 	"go-bank-backend/helpers"
+
 	"github.com/jinzhu/gorm"
-	"github.com/jinzhu/gorm/dialect/postgres"
-	"gorm.io/gorm"
-	"honnef.co/go/tools/analysis/facts/generated"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 type User struct {
-	 gorm.Model
-	 Email string `gorm:"type:varchar(100);unique_index"`
-	 Password string `gorm:"type:varchar(100)"`
-	 Username string `gorm:"type:varchar(100)"`
+	gorm.Model
+  Username string
+	Email string
+	Password string
 }
 
 type Account struct {
 	gorm.Model
-	Type string
+  Type string
 	Name string
 	Balance uint
 	UserID uint
-
 }
 
 func connectDB() *gorm.DB {
-	db, err := gorm.Open("postgres", "host=127.0.0.1 port=2002 user=postgres dbname=bank password=postgres sslmode=disable")
+	db, err := gorm.Open("postgres", "host=127.0.0.1 port=5432 user=user dbname=dbname password=password sslmode=disable")
 	helpers.HandleErr(err)
 	return db
 }
 
-func CreateAccounts() {
+// This is correct way of creating password
+// func HashAndSalt(pass []byte) string {
+// 	hashed, err := bcrypt.GenerateFromPassword(pass, bcrypt.MinCost)
+// 	helpers.HandleErr(err)
+
+// 	return string(hashed)
+// }
+
+func createAccounts() {
 	db := connectDB()
 
-	users :=[2]User{
-		{Username: "Allan" , Email:"allanmuharikamau@gmail.com"},
-		{Username: "Ezra" , Email:"Ezrakanake@gmail.com"},
+	users := [2]User{
+		{Username: "Martin", Email: "martin@martin.com"},
+		{Username: "Michael", Email: "michael@michael.com"},
 	}
-	for i := 0; i< len(users); i++{
-		generatedPassword := helpers.HashandSalt([]byte(users[i].Username))
+
+	for i := 0; i < len(users); i++ {
+		// generatedPassword := helpers.HashAndSalt([]byte(users[i].Username))
+		generatedPassword := helpers.HashOnlyVulnerable([]byte(users[i].Username))
 		user := User{Username: users[i].Username, Email: users[i].Email, Password: generatedPassword}
 		db.Create(&user)
 
-		account := Account{Type: "Daily Account", Name:string(users[i].Username + "s" + "account"), Balance: uint(1000* int(i+1)), UserID:user.ID}
+		account := Account{Type: "Daily Account", Name: string(users[i].Username + "'s" + " account"), Balance: uint(10000 * int(i+1)), UserID: user.ID}
 		db.Create(&account)
-	
-}
-defer db.Close()
+	}
+	defer db.Close()
 }
 
-func Migrate() {}
+
+func Migrate() {
+	db := connectDB()
+	db.AutoMigrate(&User{}, &Account{})
+	defer db.Close()
+	
+	createAccounts()
+}
